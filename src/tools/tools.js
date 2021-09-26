31b75e073e6346925d78e0a4b54b3a6493d7b207
halimaMowla <halimamowlaanna@gmail.com>
2021-09-26 15:53:47 +0600

    first commit

---
+++ src/tools/tools.js
@@ -0,0 +1,11 @@
+const tools = [
+    {key : 112,name:"Sakib Khan" ,age:32, monthlyIncome: 10000,bestMovie:"Hero the Superstar" ,image:"https://i.ibb.co/4SxM30S/Untitled-23-samakal-605f7d532facf.jpg"},
+    {key : 121,name:"Shuvo" ,age:28, monthlyIncome: 8000,bestMovie:"Dhaka Attack" ,image:"https://updateoffer.com/wp-content/uploads/2018/10/Arifin-Shuvo.jpg"},
+    {key : 211,name:"Bappi" ,age:26, monthlyIncome: 6000,bestMovie:"Nayok" ,image:"https://smallbiography.com/wp-content/uploads/2020/10/Bappi-chowdhori.jpg"},
+    {key : 311,name:"Joshim" ,age:40, monthlyIncome: 5000,bestMovie:"Banglar nayok" ,image:"https://www.channelionline.com/wp-content/uploads/2016/12/Actor-Jashim.jpg"},
+    {key : 312,name:"Syeam" ,age:29, monthlyIncome: 9000,bestMovie:"Poramon" ,image:"https://qph.fs.quoracdn.net/main-qimg-a64d39edb76c03a33ead494ff835e6b7"},
+    {key : 415,name:"Abdur Razzak" ,age:50, monthlyIncome: 9000,bestMovie:"Nill Akasher niche" ,image:"https://i.ibb.co/87v0wtK/Raj-Razzak-2.jpg"},
+    {key : 720,name:"Salman Shah" ,age:30, monthlyIncome: 11000,bestMovie:"Shopner Prithbi" ,image:"https://suprobhat.com/wp-content/uploads/2020/07/Salman-shah.jpg"},
+    {key : 850,name:"Omar Sani" ,age:40, monthlyIncome: 4000,bestMovie:"Tumi Amar" ,image:"https://lh3.googleusercontent.com/proxy/XEFHilx0BWdILcCrJtzSq9A3DUrtq12L1seqW9mQ16SQ4JJl_U3isbJGyaL0WDmW8E86nhcOEUBezu0Se6gta83rrLNivcY"},
+    {key : 901,name:"Ananto Jolil" ,age:32, monthlyIncome: 11000,bestMovie:"Din The Day" ,image:"https://media-eng.dhakatribune.com/uploads/2020/03/ananta-1585225073898.jpg"}
+]
